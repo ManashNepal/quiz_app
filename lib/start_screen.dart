ee0b4695653d0092e9cabccc5c31c0f3c3dfa566
ManashNepal <manash.nepal111@gmail.com>
2024-07-31 12:22:36 +0530

    adding a parent Quiz state

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -31,7 +33,7 @@ class StartScreen extends StatelessWidget {
               ),
               shadowColor: Colors.black,
             ),
-            onPressed: () {},
+            onPressed: startQuiz,
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text("Start Quiz"),
           ),
